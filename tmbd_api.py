6457f4559d1d60517497df8f7cc8efe6e58936ed
Fizmatishe <uch.nikitin@yandex.ru>
2018-03-21 23:24:28 +0300

    Add tmdb_api_wk1_stage2

--- tmbd_api.py
+++ tmbd_api.py
@@ -1,6 +1,10 @@
 import urllib.request
 import urllib.parse
 import json
+import os
+
+
+API_KEY = os.environ.get('TMDB_API_KEY')
 
 
 def load_json_data_from_url(base_url, url_params):
@@ -12,9 +16,10 @@ def load_json_data_from_url(base_url, url_params):
 def make_tmdb_api_request(method, api_key, extra_params=None):
     extra_params = extra_params or {}
     url = 'https://api.themoviedb.org/3%s' % method
-    params = {
-        'api_key': api_key,
-        'language': 'ru',
-    }
+    params = {'api_key': api_key, 'language': 'ru',}
     params.update(extra_params)
     return load_json_data_from_url(url, params)
+
+
+if __name__ == '__main__':
+    print(make_tmdb_api_request(method='/movie/215', api_key=API_KEY))
