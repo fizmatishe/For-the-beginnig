de9119fee5e70bdfd9d99461e7c0a9626210b9d1
Fizmatishe <uch.nikitin@yandex.ru>
2018-03-25 15:56:23 +0300

    Fulfilled reu_course_wk1-tmdb_api

--- recommendation_search.py
+++ recommendation_search.py
@@ -5,20 +5,27 @@ def recommend_movies(key_word):
     search_result = []
     with open('movies_base', 'r') as file:
         movies_base_list = json.load(file)
+    original_popularity = 0
+    original_vote = 0
+
     for similar_films in movies_base_list:
         if similar_films['original_title'] == key_word:
-            search_result.append(similar_films['popularity'])
-            search_result.append(similar_films)
-
-    for films in movies_base_list:
-        popularity = float(films['popularity'])
-        vote = float(films['vote_average'])
-        if ((popularity - 100 <= float(search_result[0]) <= popularity + 100) or
-                (vote - 0.3 <= search_result[1] <= vote + 0.3)):
-            search_result.append(films)
-    return set(search_result)
+            original_popularity = float(similar_films['popularity'])
+            original_vote = float(similar_films['vote_average'])
+    if original_vote == 0:
+        search_result.append('No movies were found by your search')
+        return search_result
+    else:
+        for films in movies_base_list:
+            popularity = float(films['popularity'])
+            vote = float(films['vote_average'])
+            if ((popularity - 50 <= original_popularity <=
+                popularity + 50) or
+                    (vote - 0.3 <= original_vote <= vote + 0.3)):
+                search_result.append(films)
+        return search_result
 
 
 if __name__ == '__main__':
-    input_word = input('Enter key word:  ').lower()
+    input_word = input('Enter key word:  ')
     print(recommend_movies(input_word))
